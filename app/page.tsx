a37d302b24f05db6f37b21e160b2d405688d7503
hoando <hoando.dev@gmail.com>
2025-05-24 23:58:25 +0700

    Enhance chain configuration by adding _dstEid property for mainnet, polygon, bscTestnet, and sepolia. This update improves the management of bridge and USDT addresses in the application.

--- app/page.tsx
+++ app/page.tsx
@@ -77,16 +77,18 @@ const tokenIcons: { [key: string]: string } = {
 };
 
 // Chain contract config
-export const chainConfig: Record<string, { usdt: string | null; bridge: string | null }> = {
-  mainnet: { usdt: null, bridge: null },
-  polygon: { usdt: null, bridge: null },
+export const chainConfig: Record<string, { usdt: string | null; bridge: string | null; _dstEid: number | null }> = {
+  mainnet: { usdt: null, bridge: null, _dstEid: null },
+  polygon: { usdt: null, bridge: null, _dstEid: null },
   bscTestnet: {
     usdt: "0x340Ab63e032C9354fD8d18f97833A1aB75AC1Ff7",
     bridge: "0xe71a0009716752E1d32eaE3089F4152bc5F1ebA6",
+    _dstEid: 40102,
   },
   sepolia: {
     usdt: "0x2B6069650B78b10fab9D54c9A6B6AD84b045a1CA",
     bridge: "0x212Fbda4a5B034700E1C6422880b13C9f41180FB",
+    _dstEid: 40161,
   },
 };
 
